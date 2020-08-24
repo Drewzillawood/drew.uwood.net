97e05523b779cf65ad12e5d1a7d16279274743a9
Drew Underwood <drew@uwood.net>
2020-08-24 12:27:15 -0500

    Basic structure for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { SuperHeaderComponent } from './super-header/super-header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SuperHeaderComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
