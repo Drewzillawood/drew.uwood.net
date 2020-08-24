9a9fd00c8aa7eb6869d4c553ceb70954179af5ff
Drew Underwood <drew@uwood.net>
2020-08-24 11:20:00 -0500

    Blowing away anything with material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,18 @@
-import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCardModule, MatExpansionModule} from '@angular/material';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { MenuComponent } from './toolbar/menu/menu.component';
-import { SettingsComponent } from './toolbar/settings/settings.component';
-import { AboutMeComponent } from './toolbar/settings/about-me/about-me.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import { ContactMeComponent } from './toolbar/settings/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    MenuComponent,
-    SettingsComponent,
-    AboutMeComponent,
-    ContactMeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatToolbarModule,
     AppRoutingModule,
-    MatCardModule,
-    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/toolbar/settings/about-me/model/resume-section.model.ts
+++
@@ -1,5 +0,0 @@
-export class ResumeSectionModel {
-
-  constructor(public sectionTitle: string,
-              public sectionContent: string[]) {}
-}
