13a09a8f249a169160aef0b78c33ee9d00f8caa1
Drew Underwood <drew@uwood.net>
2020-09-30 09:16:45 -0500

    Added angular logo for current project

--- src/app/content/introduction/introduction.component.ts
+++ src/app/content/introduction/introduction.component.ts
@@ -6,8 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
-  intro: string = `Experienced Software Associate with a demonstrated history of working in the computer software industry.
-                   Skilled in Java, SQL, MVC/MVP Frameworks and Full-Stack Development. Eager to pursue new
+  intro: string = `Experienced Software Engineer with a demonstrated history of working in the computer software industry.\
+                   Skilled in Java, SQL, MVC/MVP Frameworks and Full-Stack Development. Eager to pursue new\
                    opportunities to further my understanding of application and web development.`;
 
   constructor() { }
