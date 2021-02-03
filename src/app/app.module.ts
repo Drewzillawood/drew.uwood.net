c3f3004c85bdbd1861ce091367340c93a46b8c07
Drew Underwood <drew@uwood.net>
2021-02-03 09:45:21 -0600

    Simple path drawn for initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import { DotComponent } from './dot/dot.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   imports: [
-
+    BrowserModule
   ],
   declarations: [
     AppComponent,
