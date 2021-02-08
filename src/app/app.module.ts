1aaf54f3398ce279000e3f13980348b8043eebcf
Drew Underwood <drew@uwood.net>
2021-02-08 14:24:44 -0600

    NGRX Dev Tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AppComponent} from './app.component';
 import { NodeComponent } from './node/node.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import * as fromApp from './store/app.reducer';
 import { NodeHeadComponent } from './node/node-head/node-head.component';
@@ -11,6 +12,7 @@ import { NodeBodyComponent } from './node/node-body/node-body.component';
 
 @NgModule({
   imports: [
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     BrowserModule,
     StoreModule.forRoot(fromApp.appReducer)
   ],
