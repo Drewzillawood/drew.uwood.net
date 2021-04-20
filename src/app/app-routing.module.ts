beaaf0221c30aba7b7537138ebf791470145a298
Drew Underwood <drew@uwood.net>
2021-04-19 19:05:48 -0500

    Deploy fix 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { AppComponent } from './app.component';
-import { SuperHeaderComponent } from './super-header/super-header.component';
 
 const appRoutes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
